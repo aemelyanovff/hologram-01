4918e878ffe3e929f420b7694c6e51792540b445
Hologram <hologram@dreamflow.com>
2025-09-16 12:23:34 -0400

    My Commit Message

--- lib/pages/home_page/home_page_widget.dart
+++ lib/pages/home_page/home_page_widget.dart
@@ -50,7 +50,7 @@ class _HomePageWidgetState extends State<HomePageWidget> {
           backgroundColor: FlutterFlowTheme.of(context).primary,
           automaticallyImplyLeading: false,
           title: Text(
-            "Page Title branch 6",
+            "Page Title branch 7",
             style: FlutterFlowTheme.of(context).headlineMedium.override(
                   font: GoogleFonts.interTight(
                     fontWeight:
